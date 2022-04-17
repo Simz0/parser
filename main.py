6fa4b1be7d1e1663024412b92a6e4873d5fe3423
Simz0 <103265033+Simz0@users.noreply.github.com>
2022-04-17 15:55:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
+namereciptes = []
+reciptes = []
+
 url = "https://eda.ru/recepty"
 page = requests.get(url)
 print(page.status_code)
-namereciptes = []
-reciptes = []
+
 soup = BeautifulSoup(page.text, "html.parser")
 allreciptes = soup.findAll('a', class_="emotion-18hxz5k")
-print(allreciptes)
+
+
 for data in allreciptes:
     if data.find('span', class_="emotion-1j2opmb"):
         namereciptes.append(data.text)
+        
 print(namereciptes)
+
 for data in namereciptes:
     print(data)
