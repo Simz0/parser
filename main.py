02204bd91fa3be248c96b51badfff991001e6e38
BigCubeCat <bigcubecat@yandex.ru>
2022-04-19 10:18:42 +0700

    Find all title and links

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ url = "https://eda.ru/recepty"
 page = requests.get(url)
 print(page.status_code)
 
-soup = BeautifulSoup(page.text, "html.parser")
-allreciptes = soup.findAll('a', class_="emotion-18hxz5k")
+
+def get_page_recipes(page_text: str):
+    soup = BeautifulSoup(page_text, "html.parser")
+    allreciptes = soup.findAll('a', class_="emotion-18hxz5k")
+    result = []
+    for data in allreciptes:
+        if data.find('span', class_="emotion-1j2opmb"):
+            result.append(str((data.text, data.attrs["href"])))
+    return result
 
 
-for data in allreciptes:
-    if data.find('span', class_="emotion-1j2opmb"):
-        namereciptes.append(data.text)
-        
-print(namereciptes)
-
-for data in namereciptes:
-    print(data)
+for i in range(1, 715):
+    page_text = ""
+    try:
+        with open(f"pages/{i}.html") as f:
+            page_text = '\n'.join(f.readlines())
+    except Exception as error:
+        print(error)
+    if page_text != "":
+        rec = "\n".join(get_page_recipes(page_text))
+        with open('result.txt', 'a') as f:
+            f.write(rec)
