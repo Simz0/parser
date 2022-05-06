6250c6e0a60515cf6b14796f0cbb22e465772d32
BigCubeCat <bigcubecat@yandex.ru>
2022-05-06 17:02:56 +0700

    fix link

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 import requests
 
@@ -11,7 +12,7 @@ def log(data):
         log_file.write(data)
 
 
-FOLDER = 'recipes'
+FOLDER = '/home/bigcubecat/answer/recipes'
 
 if __name__ == "__main__":
     recipes = {}
@@ -19,27 +20,30 @@ if __name__ == "__main__":
         k, v = eval(obj)
         recipes[k] = v
     for filename in os.listdir(FOLDER):
-        with open(f'{FOLDER}/{filename}') as f:
-            page_text = '\n'.join(f.readlines())
-        title = filename.replace(".html", "")
-        description = get_description(page_text)
-        ingredient_titles, counts = get_ingredients(page_text)
-        energy = ';'.join(get_energy(page_text))  # соеденины ;
-        steps = ';'.join(get_steps(page_text))
-        counts_json = {}
-        for i, e in enumerate(counts):
-            counts_json[ingredient_titles[i]] = e
-        link = recipes[title]
-        print(ingredient_titles)
-        new_recipe = {
-            "title": title,
-            "description": description,
-            "energy": energy,
-            "steps": steps,
-            "ingredients": ingredient_titles,
-            "counts": json.dumps(counts_json),
-            "link": link
-        }
-        response = requests.post('http://127.0.0.1:8080/add', json=new_recipe)
-        print(new_recipe)
-        print(response.text)
+        time.sleep(1)
+        try:
+            with open(f'{FOLDER}/{filename}') as f:
+                page_text = '\n'.join(f.readlines())
+            title = filename.replace(".html", "")
+            description = get_description(page_text)
+            ingredient_titles, counts = get_ingredients(page_text)
+            energy = ';'.join(get_energy(page_text))  # соеденины ;
+            steps = ';'.join(get_steps(page_text))
+            counts_json = {}
+            for i, e in enumerate(counts):
+                counts_json[ingredient_titles[i]] = e
+            link = recipes[title]
+            new_recipe = {
+                "title": title.lower(),
+                "description": description,
+                "energy": energy,
+                "steps": steps,
+                "ingredients": ingredient_titles,
+                "counts": json.dumps(counts_json),
+                "link": f"https://eda.ru{link}"
+            }
+            response = requests.post('http://127.0.0.1:8080/add', json=new_recipe)
+            if response.status_code != 200:
+                log(str(response) + "\n\n\n")
+        except Exception as error:
+            log(str(error) + "\n\n\n")
